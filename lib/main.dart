5aa57b13c2a54cac75b1cfc5a38a7e4d548f532b
Asem Qadi <asem5141@gmail.com>
2026-03-03 12:40:57 +0200

    re-structure project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:book_store/features/onboarding/onboarding_screen.dart';
+import 'package:book_store/presentation/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
