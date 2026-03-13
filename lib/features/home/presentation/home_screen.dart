e26d88684e419762ec63225063d19fee7758628c
Asem Qadi <asem5141@gmail.com>
2026-03-14 00:06:19 +0200

    feat: add service locator and cubits for managing featured and newest books; update dependencies in pubspec

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,3 +1,9 @@
+import 'package:book_store/core/utils/service_locator.dart';
+import 'package:book_store/features/home/data/repo/home_repo_impl.dart';
+import 'package:book_store/features/home/presentation/cubit/featured_books/featured_books_cubit.dart';
+import 'package:book_store/features/home/presentation/cubit/newest_books/newest_books_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../core/helper/spacing.dart';
 import 'widgets/best_seller_books.dart';
 import 'widgets/custom_app_bar.dart';
@@ -10,30 +16,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    CustomAppBar(),
-                    verticalSpace(25),
-                    const SectionHeader(title: 'Discover Books'),
-                    verticalSpace(10),
-                    const DiscoveringBooks(),
-                    verticalSpace(10),
-                    const SectionHeader(title: 'Best Seller'),
-                    verticalSpace(10),
-                  ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      CustomAppBar(),
+                      verticalSpace(25),
+                      const SectionHeader(title: 'Discover Books'),
+                      verticalSpace(10),
+                      const DiscoveringBooks(),
+                      verticalSpace(10),
+                      const SectionHeader(title: 'Best Seller'),
+                      verticalSpace(10),
+                    ],
+                  ),
                 ),
-              ),
 
-              const BestSellerBooks(),
-            ],
+                const BestSellerBooks(),
+              ],
+            ),
           ),
         ),
       ),
